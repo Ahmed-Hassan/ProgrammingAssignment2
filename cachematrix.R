fe016e49a7b0c74421202d575d9ab5898ead8765
Ahmed-Hassan <ahmed.adel50@gmail.com>
2015-03-14 22:12:10 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,41 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(data.mat = matrix()) {
+	inverse <- NULL
+	dot.args <- NULL
+	set <- function(new.mat) {
+		data.mat <<- new.mat
+		inverse <<- NULL
+		dot.args <<- NULL
+	}
+	get <- function() {
+		data.mat
+	}
+	set.inv <- function(...) {
+		inverse <<- solve(data.mat, ...)
+		dot.args <<- list(...)
+	}
+	get.inv <- function() {
+		inverse
+	}
+	get.dot.args <- function () {
+		dot.args
+	}
+	list(set = set, get = get, set.inv = set.inv, get.inv = get.inv, get.dot.args)
 
 }
 
+## Return a matrix that is the inverse of 'x'
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(mat.so, ...) {
+	this.dot.args <- list(...)
+	cached.dot.args <- mat.so$get.dot.args()
+	cached.inv <- mat.so$get.inv()
+	if(!is.null(cached.inv) && sum(!(this.dot.args %in% cached.dot.args) == 0) {
+		message("Found it cached and calculated with same dot arguments")
+		return(cached.inv)
+	}
+	mat.so$set.inv(...)
+	mat.so$get.inv()
 }
